f68b786fe16c6b274a1ffa21446e72be8c8cfead
Olaf Merkert <olaf.merkert@tngtech.com>
2018-08-22 11:34:16 +0200

    refactor template selection logic

--- src/automaticCaptureTemplateSelection.js
+++ src/automaticCaptureTemplateSelection.js
@@ -1,28 +1,37 @@
+import { assoc, flip, test } from "ramda";
+
 const CAPTURE_TEMPLATE_KEYS = {
   PULL_REQUEST: "R",
   JIRA_TASK: "J",
 };
 
+const jiraBoardParameters = parameters => {
+  const boardUrl = new URL(parameters.url);
+  const taskId = boardUrl.searchParams.get("selectedIssue");
+  const url = new URL(`/browse/${taskId}`, parameters.url).toString();
+  return {
+    ...parameters,
+    template: CAPTURE_TEMPLATE_KEYS.JIRA_TASK,
+    title: `JIRA ${taskId}`,
+    url,
+  };
+};
+
 export const detectCaptureTemplate = parameters => {
+  const testUrl = flip(test)(parameters.url);
+  const withTemplate = flip(assoc("template"))(parameters);
+
   const tfsPRPattern = /tfs.*pullrequest/;
-  if (tfsPRPattern.test(parameters.url)) {
-    return { ...parameters, template: CAPTURE_TEMPLATE_KEYS.PULL_REQUEST };
+  if (testUrl(tfsPRPattern)) {
+    return withTemplate(CAPTURE_TEMPLATE_KEYS.PULL_REQUEST);
   }
   const jiraIssuePattern = /jira.*browse/;
-  if (jiraIssuePattern.test(parameters.url)) {
-    return { ...parameters, template: CAPTURE_TEMPLATE_KEYS.JIRA_TASK };
+  if (testUrl(jiraIssuePattern)) {
+    return withTemplate(CAPTURE_TEMPLATE_KEYS.JIRA_TASK);
   }
   const jiraBoardPattern = /jira.*selectedIssue/;
-  if (jiraBoardPattern.test(parameters.url)) {
-    const boardUrl = new URL(parameters.url);
-    const taskId = boardUrl.searchParams.get("selectedIssue");
-    const url = new URL(`/browse/${taskId}`, parameters.url).toString();
-    return {
-      ...parameters,
-      template: CAPTURE_TEMPLATE_KEYS.JIRA_TASK,
-      title: `JIRA ${taskId}`,
-      url,
-    };
+  if (testUrl(jiraBoardPattern)) {
+    return jiraBoardParameters(parameters);
   }
   return parameters;
 };
