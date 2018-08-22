d8a1674128b79d1ba41289c71071c554e3b90c4b
Olaf Merkert <olaf.merkert@tngtech.com>
2018-08-22 11:24:27 +0200

    detect jira issue or board with selected issue

--- src/automaticCaptureTemplateSelection.js
+++ src/automaticCaptureTemplateSelection.js
@@ -1,11 +1,28 @@
-const TEMPLATE_KEYS = {
+const CAPTURE_TEMPLATE_KEYS = {
   PULL_REQUEST: "R",
+  JIRA_TASK: "J",
 };
 
 export const detectCaptureTemplate = parameters => {
   const tfsPRPattern = /tfs.*pullrequest/;
   if (tfsPRPattern.test(parameters.url)) {
-    return { ...parameters, template: TEMPLATE_KEYS.PULL_REQUEST };
+    return { ...parameters, template: CAPTURE_TEMPLATE_KEYS.PULL_REQUEST };
+  }
+  const jiraIssuePattern = /jira.*browse/;
+  if (jiraIssuePattern.test(parameters.url)) {
+    return { ...parameters, template: CAPTURE_TEMPLATE_KEYS.JIRA_TASK };
+  }
+  const jiraBoardPattern = /jira.*selectedIssue/;
+  if (jiraBoardPattern.test(parameters.url)) {
+    const boardUrl = new URL(parameters.url);
+    const taskId = boardUrl.searchParams.get("selectedIssue");
+    const url = new URL(`/browse/${taskId}`, parameters.url).toString();
+    return {
+      ...parameters,
+      template: CAPTURE_TEMPLATE_KEYS.JIRA_TASK,
+      title: `JIRA ${taskId}`,
+      url,
+    };
   }
   return parameters;
 };
--- src/automaticCaptureTemplateSelection.test.js
+++ src/automaticCaptureTemplateSelection.test.js
@@ -25,4 +25,32 @@ describe("The automatic selection of a capture template", () => {
     const actualParameters = detectCaptureTemplate(exampleParameters);
     expect(actualParameters).toEqual({ ...exampleParameters, template: "R" });
   });
+
+  it("detects a JIRA issue page base on the URL", () => {
+    const exampleParameters = {
+      title: "exampleTitle",
+      url: "https://jira.somewhere.com/browse/TM-3479",
+      body: "exampleSelection",
+    };
+
+    const actualParameters = detectCaptureTemplate(exampleParameters);
+    expect(actualParameters).toEqual({ ...exampleParameters, template: "J" });
+  });
+
+  it("detects a JIRA issue page base on the URL", () => {
+    const exampleParameters = {
+      title: "exampleTitle",
+      url:
+        "https://jira.somewhere.com/secure/RapidBoard.jspa?rapidView=100&view=detail&selectedIssue=TM-3479",
+      body: "exampleSelection",
+    };
+
+    const actualParameters = detectCaptureTemplate(exampleParameters);
+    expect(actualParameters).toEqual({
+      title: "JIRA TM-3479",
+      url: "https://jira.somewhere.com/browse/TM-3479",
+      body: "exampleSelection",
+      template: "J",
+    });
+  });
 });
