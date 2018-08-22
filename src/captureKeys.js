fdae24b9728a3b5568d947ffb755a272c122537a
Olaf Merkert <olaf.merkert@tngtech.com>
2018-08-22 13:21:03 +0200

    logic for handling command triggered by keys

--- src/captureKeys.js
+++ src/captureKeys.js
@@ -1,1 +1,26 @@
-console.log("Hello background");
+import {
+  handleAutomaticCapture,
+  handleStoreLink,
+  handleOpenInEmacsBrowser,
+} from "./protocolInvocation";
+
+const COMMANDS = {
+  automaticCapture: "automatic-capture",
+  storeLink: "store-link",
+  openInEmacsBrowser: "open-in-emacs-browser",
+};
+
+const keyCommandHandler = command => {
+  switch (command) {
+    case COMMANDS.automaticCapture:
+      return handleAutomaticCapture();
+    case COMMANDS.storeLink:
+      return handleStoreLink();
+    case COMMANDS.openInEmacsBrowser:
+      return handleOpenInEmacsBrowser();
+    default:
+      return undefined;
+  }
+};
+
+browser.commands.onCommand.addListener(keyCommandHandler);
