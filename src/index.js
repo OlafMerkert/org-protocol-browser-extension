45641678fe9270f013b6560dfdba81ec6c93fbc5
Olaf Merkert <olaf.merkert@tngtech.com>
2018-08-22 09:23:24 +0200

    copy selection action

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
 import { getActiveTab, getSelectionFromTab, getUrlAndTitleFromTab } from "./activeTabData";
-import { capture, storeLink } from "./org";
+import { capture, storeLink, copySelection } from "./org";
 
 const handleCapture = async () => {
   const activeTab = await getActiveTab();
@@ -16,6 +16,12 @@ const handleStoreLink = async () => {
   await storeLink(urlAndTitle);
 };
 
+const handleCopySelection = async () => {
+  const activeTab = await getActiveTab();
+  const selection = await getSelectionFromTab(activeTab);
+  await copySelection({ selection });
+};
+
 const App = props => (
   <div>
     <div>
@@ -23,6 +29,9 @@ const App = props => (
     </div>
     <div>
       <button onClick={handleStoreLink}>Store Link</button>
+    </div>
+    <div>
+      <button onClick={handleCopySelection}>Copy Selection</button>
     </div>
   </div>
 );
