b137f5798559120a74d93af2ddfe7c0df073d38b
Olaf Merkert <olaf.merkert@tngtech.com>
2018-08-22 12:17:19 +0200

    configure loading css

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   module: {
-    rules: [{ test: /\.jsx?$/, exclude: /node_modules/, use: "babel-loader" }, { test: /\.css$/ }],
+    rules: [
+      { test: /\.jsx?$/, exclude: /node_modules/, use: "babel-loader" },
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+    ],
   },
   devtool: "source-map",
 };
