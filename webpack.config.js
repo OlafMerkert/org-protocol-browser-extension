8e68c19d374dfbf961a9f27b58125f75d563e034
Olaf Merkert <olaf.merkert@tngtech.com>
2018-08-22 15:11:26 +0200

    configure webpack to generate html file for popup

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,9 @@
+const path = require("path");
 const Visualizer = require("webpack-visualizer-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const distFolder = path.resolve(__dirname, "target", "dist");
 
 module.exports = {
   module: {
@@ -8,9 +13,21 @@ module.exports = {
     ],
   },
   devtool: "source-map",
-  plugins: [new Visualizer({ filename: "./bundle-size-report.html" })],
+  plugins: [
+    new CleanWebpackPlugin([distFolder]),
+    new HtmlWebpackPlugin({
+      title: "Org Protocol Browser Extension",
+      filename: "capturePopup.html",
+    }),
+    new Visualizer({
+      filename: path.join("..", "report", "bundle-size-report.html"),
+    }),
+  ],
   entry: {
     capturePopup: "./src/capturePopup.js",
     captureKeys: "./src/captureKeys.js",
+  },
+  output: {
+    path: distFolder,
   },
 };
