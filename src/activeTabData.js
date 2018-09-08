3e122f9388bf8fe661f0ef0c5b9614395814b8fe
Olaf Merkert <olaf.merkert@tngtech.com>
2018-09-08 21:50:00 +0200

    simplify browser switch

--- src/activeTabData.js
+++ src/activeTabData.js
@@ -1,42 +1,28 @@
 import { join, pick } from "ramda";
 
-const getActiveTabFirefox = async () => {
-  const tabs = await browser.tabs.query({
+const getCurrentWindowId = () => (window.browser || chrome).windows.WINDOW_ID_CURRENT;
+
+const tabsQuery = window.browser
+  ? browser.tabs.query
+  : payload => new Promise(resolve => chrome.tabs.query(payload, resolve));
+
+const tabExecuteScript = window.browser
+  ? browser.tabs.executeScript
+  : (tabId, payload) => new Promise(resolve => chrome.tabs.executeScript(tabId, payload, resolve));
+
+export const getActiveTab = async () => {
+  const tabs = await tabsQuery({
     active: true,
-    windowId: browser.windows.WINDOW_ID_CURRENT,
+    windowId: getCurrentWindowId(),
   });
   return tabs[0];
 };
 
-const getActiveTabChrome = () =>
-  new Promise((resolve, reject) => {
-    chrome.tabs.query(
-      {
-        active: true,
-        windowId: chrome.windows.WINDOW_ID_CURRENT,
-      },
-      tabs => resolve(tabs[0])
-    );
-  });
-
-export const getActiveTab = window.browser ? getActiveTabFirefox : getActiveTabChrome;
-
 export const getUrlAndTitleFromTab = pick(["url", "title"]);
 
-const getSelectionFromTabFirefox = async tab => {
-  const selection = await browser.tabs.executeScript(tab.id, {
+export const getSelectionFromTab = async tab => {
+  const selection = await tabExecuteScript(tab.id, {
     code: "getSelection().toString();",
   });
   return join("", selection);
 };
-
-const getSelectionFromTabChrome = tab =>
-  new Promise((resolve, reject) => {
-    chrome.tabs.executeScript(tab.id, { code: "getSelection().toString();" }, selection =>
-      resolve(join("", selection))
-    );
-  });
-
-export const getSelectionFromTab = window.browser
-  ? getSelectionFromTabFirefox
-  : getSelectionFromTabChrome;
